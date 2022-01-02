00ee4d2a68c29f49fc2e2f62c0f1f2923580d758
Sophia Kim <mail@fifikim.com>
2022-01-01 22:34:57 -0500

    Replace comments with docstrings specifying param & return types

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import math
 
 # CLASSES
 
-# prints menu with options for current view & executes user-selected action
 class Menu:
+    '''This class generates page menus and executes user selections of menu options.'''
     def __init__(self, options, results=[]):
         self.options = options
         self.results = results
@@ -41,6 +41,7 @@ class Menu:
         }
 
     def print(self):
+        '''Print menu with available options for current view.'''
         print(style_output('\n\nWhat would you like to do?', 'underline'))
         for (i, element) in enumerate(self.options, start=1):
             label = self.options_dict[element]['label']
@@ -50,6 +51,7 @@ class Menu:
         self.select()
 
     def select(self):
+        '''Prompt user for selection and execute selected action.'''
         selection = input('Please enter your selection:  ')
         valid = validate_selection(selection, self.options)
 
@@ -64,12 +66,13 @@ class Menu:
                 f'Invalid selection. Please choose from Options #1-{len(self.options)}.\n', 'warning'))
             self.select()
 
-# prints header for current view
 class Header:
+    '''This class generates decorative page headers.'''
     def __init__(self, name):
         self.name = name.upper()
 
     def print(self):
+        '''Print page header for current view.'''
         margin = math.floor((35 - len(self.name)) / 2) * ' '
         border = '=' * 30
         border2 = '=' * 34
@@ -80,24 +83,41 @@ class Header:
         print(f" {style_output(border2, 'border')}")
         print(f'   {border}\n\n')
 
-# stores book data as object & prints formatted details
 class Book:
+    '''This class handles book items.'''
     def __init__(self, title, author, publisher):
         self.title = title
         self.author = author
         self.publisher = publisher
 
     def print(self):
+        '''prints formatted details for selected book'''
         title = style_output(self.title, 'title')
         print(f'    Title: {title}')
         print(f'    Author(s): {self.author}')
         print(f'    Publisher: {self.publisher}')
 
+    def write_to_saved(self):
+        '''saves selected book to reading list'''
+        pass
+
+class List:
+    '''This class handles lists of books.'''
+    def __init__(self, name):
+        self.name = name
 
 # SHARED UTILITIES
 
-# validates user selection is available menu option
 def validate_selection(val, list):
+    '''Validate that a user selection is an available menu option.
+    
+    :param val: User-selected input.
+    :type val: str
+    :param list: List of available menu options.
+    :type list: list
+    :return: True if selection is valid, else False.
+    :rtype: boolean
+    '''
     max = len(list)
     if val:
         try:
@@ -109,14 +129,30 @@ def validate_selection(val, list):
         except ValueError:
             return False
 
-# formats books in a numbered list
 def display_books(list):
+    '''Print list of books as a numbered, formatted list.
+        
+    :param val: 
+    :type val: 
+    :param list: 
+    :type list:
+    :return: 
+    :rtype: 
+    '''
     for (i, book) in enumerate(list, start=1):
         print(style_output(f'ID {i}', 'header'))
         book.print()
 
-# applies text styling to terminal output
 def style_output(string, style):
+    '''Apply text styling to terminal output.
+        
+    :param val: 
+    :type val: 
+    :param list: 
+    :type list: 
+    :return: 
+    :rtype: 
+    '''
     reset = '\033[0;0m'
     styles = {
         'header': '\033[1;36m',
@@ -131,35 +167,54 @@ def style_output(string, style):
 
 # SEARCH VIEW
 
-# displays search header & prompts user for query
 def search():
+    '''Display search header & prompts user for query.'''
     header = Header('search')
     header.print()
     # TO DO: add escape key to cancel query
-    q = validate_query()
+    q = prompt_query()
     fetched = fetch_by(q)
     display_results(fetched)
 
-# validates search query, repeats prompt if query is blank
-def validate_query():
+def prompt_query():
+    '''Prompt user to input search query.
+    
+    Repeat prompt if user tries to enter a blank query.
+        
+    :return: User-inputted query term(s).
+    :rtype: str
+    '''
     query = input('Search for books containing the query:  ')
+    # TO-DO: refactor if/else to try/except so that this function has one return type
     if query:
         return query
     else:
         print(style_output('Please enter a valid query.\n', 'warning'))
-        validate_query()
+        prompt_query()
 
-# submits api get request & returns relevant data from response
 def fetch_by(query):
+    '''Submit API get request & return response.
+            
+    :param query: User-submitted search query.
+    :type query: str
+    :return: List of Book class objects representing search results. 
+    :rtype: list
+    '''
     response = requests.get(
         f'https://www.googleapis.com/books/v1/volumes?q={query}&maxResults=5').json()
     if response['totalItems'] == 0:
-        return False
-    search_results = (format_search_results(response))
-    return search_results
+        return []
+    else:
+        return format_search_results(response)
 
-# formats response data & saves as Book instance
 def format_search_results(data):
+    '''Extract relevant information from API response data & save items as Book instances.
+               
+    :param data: User-submitted search query.
+    :type data: str
+    :return: List of Book objects representing search results. 
+    :rtype: list
+    '''
     results = []
     for item in data['items']:
         if 'title' not in item['volumeInfo']:
@@ -178,9 +233,15 @@ def format_search_results(data):
         results.append(result)
     return results
 
-# prints formatted search results & new menu selections
 def display_results(results):
-    if results == False:
+    '''Print formatted search results & new menu options.
+                   
+    :param results: List of Book objects representing search results.
+    :type results: list
+    :return: List of Book class instances representing search results. 
+    :rtype: list
+    '''
+    if results == []:
         print(style_output('Sorry, your search returned 0 results.', 'warning'))
         menu = Menu(['new', 'view', 'exit'])
         menu.print()
@@ -190,8 +251,12 @@ def display_results(results):
         menu = Menu(['save', 'new', 'view', 'exit'], results)
         menu.print()
 
-# saves selected book to reading list
 def save(search_results):
+    '''Save selected book to reading list.
+    
+    :param search_results: List of Book objects representing search results.
+    :type search_results: list
+    '''
     selection = input('Please enter the ID of the book to save:   ')
     valid = validate_selection(selection, search_results)
     if valid:
@@ -206,19 +271,22 @@ def save(search_results):
             f'Invalid selection. Please choose from IDs #1-{len(search_results)}.\n', 'warning'))
         save(search_results)
 
-# appends book data to reading_list.json file
 def write_to_saved(book):
+    '''Append book data to reading_list.json file.
+        
+    :param book: Selected search result.
+    :type book: 
+    '''
     with open('reading_list.json', 'r+') as file:
         file_data = json.load(file)
         file_data["reading_list"].append(book)
         file.seek(0)
         json.dump(file_data, file, indent=4)
 
-
 # READING LIST VIEW
 
-# displays reading list header & prints saved books
 def view_reading_list():
+    '''Display reading list header & print saved books.'''
     header = Header('reading list')
     header.print()
     list = load_saved()
@@ -230,14 +298,14 @@ def view_reading_list():
     menu = Menu(['search', 'exit'])
     menu.print()
 
-# loads data from reading_list.json file 
 def load_saved():
+    '''Load data from reading_list.json file.'''
     with open('reading_list.json') as f:
       data = json.load(f)
       return data['reading_list']
 
-# formats JSON strings as list of Book instances
 def format_loaded(list):
+    '''Format JSON strings as a list of Book instances.'''
     books = []
     for record in list:
         book = json.loads(record)
@@ -247,8 +315,8 @@ def format_loaded(list):
 
 # QUIT VIEW
 
-# displays quit header & goodbye message
 def quit():
+    '''Displays quit header & goodbye message.'''
     quit_header = Header('quit')
     quit_header.print()
     print(style_output('\nThanks for using Books on 8th! Goodbye.\n', 'success'))
@@ -256,8 +324,8 @@ def quit():
 
 # HOMEPAGE VIEW
 
-# displays homepage header & menu
 def main():
+    '''Displays homepage header & menu.'''
     header = Header('home')
     header.print()
     print(style_output('      Welcome to Books on 8th!', 'header'))
